dad1d87abf2121c7f135a858398435ba9c6c0aa8
Muhayyo00 <muhayyohojiboeva86@gmail.com>
2024-02-05 15:17:33 -0700

    PracticeCucumber0205.3

---
+++ src/test/java/utils/ConfigReader.java
@@ -0,0 +1,28 @@
+package utils;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+
+
+    public static String read(String key)  {
+        return read(key, Constants.CONFIG_FILE_PATH);
+    }
+// in these method "read" we use method of overloading
+// method overloading-->Concept of static polymorphism
+    public static String read(String key, String path)  {
+
+        Properties  properties = new Properties();
+        try (FileInputStream fis = new FileInputStream(path))
+        {
+            properties.load(fis);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        return properties.getProperty(key);
+
+
+
+    }
+}
---
+++ src/test/java/utils/Constants.java
@@ -0,0 +1,13 @@
+package utils;
+
+
+    public class Constants {
+        public static final String CONFIG_FILE_PATH="C:\\Users\\muhay\\IdeaProjects\\SDETJavaBatch18\\Files\\Config.properties";
+public static final int EXPLICIT_WAIT=20;
+public static final int IMPLICIT_WAIT=10;
+
+
+
+
+
+}
